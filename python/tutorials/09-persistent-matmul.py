d14716450b73fef3487dac9fcb45d33111164ff7
Elliot Gorokhovsky <embg@fb.com>
2024-07-11 16:53:17 -0700

    override infrastructure

--- python/tutorials/09-persistent-matmul.py
+++ python/tutorials/09-persistent-matmul.py
@@ -306,29 +306,79 @@ def matmul_kernel_tma_persistent(a_desc_ptr, b_desc_ptr, c_desc_ptr,  #
 
             tl._experimental_descriptor_store(c_desc_ptr, c, [offs_am, offs_bn])
             accumulator = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)
+            
+            
+@triton.jit(launch_metadata=_matmul_launch_metadata)
+def matmul_kernel_tma_persistent_OVERRIDE(a_desc_ptr, b_desc_ptr, c_desc_ptr,  #
+                                 M, N, K,  #
+                                 BLOCK_SIZE_M: tl.constexpr,  #
+                                 BLOCK_SIZE_N: tl.constexpr,  #
+                                 BLOCK_SIZE_K: tl.constexpr,  #
+                                 GROUP_SIZE_M: tl.constexpr,  #
+                                 FP8_OUTPUT: tl.constexpr,  #
+                                 NUM_SMS: tl.constexpr):  #
+    dtype = tl.float8e4nv if FP8_OUTPUT else tl.float16
+    start_pid = tl.program_id(axis=0)
+    num_pid_m = tl.cdiv(M, BLOCK_SIZE_M)
+    num_pid_n = tl.cdiv(N, BLOCK_SIZE_N)
+    k_tiles = tl.cdiv(K, BLOCK_SIZE_K)
+    num_tiles = num_pid_m * num_pid_n
 
+    tiles_per_SM = num_tiles // NUM_SMS
+    if start_pid < num_tiles % NUM_SMS:
+        tiles_per_SM += 1
 
-def matmul_tma_persistent(a, b):
-    # Autotuner does not work with TMA. Use manual config.
-    configs = {
-        torch.float8_e4m3fn: {
-            "BLOCK_SIZE_M": 128, "BLOCK_SIZE_N": 256, "BLOCK_SIZE_K": 128, "GROUP_SIZE_M": 8, "num_stages": 4,
-            "num_warps": 8
-        }, torch.float16: {
-            "BLOCK_SIZE_M": 128, "BLOCK_SIZE_N": 256, "BLOCK_SIZE_K": 64, "GROUP_SIZE_M": 8, "num_stages": 3,
-            "num_warps": 8
-        }
+    tile_id = start_pid - NUM_SMS
+    ki = -1
+
+    pid_m = 0
+    pid_n = 0
+    offs_am = 0
+    offs_bn = 0
+
+    num_pid_in_group = GROUP_SIZE_M * num_pid_n
+
+    accumulator = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)
+
+    for _ in range(0, k_tiles * tiles_per_SM):
+        ki = tl.where(ki == k_tiles - 1, 0, ki + 1)
+        if ki == 0:
+            tile_id += NUM_SMS
+            group_id = tile_id // num_pid_in_group
+            first_pid_m = group_id * GROUP_SIZE_M
+            group_size_m = min(num_pid_m - first_pid_m, GROUP_SIZE_M)
+            pid_m = first_pid_m + (tile_id % group_size_m)
+            pid_n = (tile_id % num_pid_in_group) // group_size_m
+
+            offs_am = pid_m * BLOCK_SIZE_M
+            offs_bn = pid_n * BLOCK_SIZE_N
+
+        offs_k = ki * BLOCK_SIZE_K
+
+        a = tl._experimental_descriptor_load(a_desc_ptr, [offs_am, offs_k], [BLOCK_SIZE_M, BLOCK_SIZE_K], dtype)
+        b = tl._experimental_descriptor_load(b_desc_ptr, [offs_bn, offs_k], [BLOCK_SIZE_N, BLOCK_SIZE_K], dtype)
+        accumulator = tl.dot(a, b.T, accumulator)
+
+        if ki == k_tiles - 1:
+            c = accumulator.to(dtype)
+
+            tl._experimental_descriptor_store(c_desc_ptr, c, [offs_am, offs_bn])
+            accumulator = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)
+
+                
+# Autotuner does not work with TMA. Use manual config.
+configs = {
+    torch.float8_e4m3fn: {
+        "BLOCK_SIZE_M": 128, "BLOCK_SIZE_N": 256, "BLOCK_SIZE_K": 128, "GROUP_SIZE_M": 8, "num_stages": 4,
+        "num_warps": 8
+    }, torch.float16: {
+        "BLOCK_SIZE_M": 128, "BLOCK_SIZE_N": 256, "BLOCK_SIZE_K": 64, "GROUP_SIZE_M": 8, "num_stages": 3,
+        "num_warps": 8
     }
+}
 
-    # Check constraints.
-    assert a.shape[1] == b.shape[1], "Incompatible dimensions"  # b is transposed
-    assert a.dtype == b.dtype, "Incompatible dtypes"
-
-    M, K = a.shape
-    N, K = b.shape
-    dtype = a.dtype
-
-    c = torch.zeros((M, N), device=a.device, dtype=dtype)
+                                 
+def tma_setup(a, b, c, M, N, K):
     desc_a = triton.tools.experimental_descriptor.create_2d_tma_descriptor(a.data_ptr(), M, K,
                                                                            configs[dtype]["BLOCK_SIZE_M"],
                                                                            configs[dtype]["BLOCK_SIZE_K"],
@@ -341,21 +391,49 @@ def matmul_tma_persistent(a, b):
                                                                            configs[dtype]["BLOCK_SIZE_M"],
                                                                            configs[dtype]["BLOCK_SIZE_N"],
                                                                            c.element_size())
+    return (desc_a, desc_b, desc_c)
+
+
+def matmul_tma_persistent(a, b, c, desc_a, desc_b, desc_c, override = False):
+    # Check constraints.
+    assert a.shape[1] == b.shape[1], "Incompatible dimensions"  # b is transposed
+    assert a.dtype == b.dtype, "Incompatible dtypes"
+
+    M, K = a.shape
+    N, K = b.shape
+    dtype = a.dtype
+
     NUM_SMS = torch.cuda.get_device_properties("cuda").multi_processor_count
 
     grid = lambda META: (min(NUM_SMS, triton.cdiv(M, META["BLOCK_SIZE_M"]) * triton.cdiv(N, META["BLOCK_SIZE_N"])), )
-    matmul_kernel_tma_persistent[grid](
-        desc_a, desc_b, desc_c,  #
-        M, N, K,  #
-        BLOCK_SIZE_M=configs[dtype]["BLOCK_SIZE_M"],  #
-        BLOCK_SIZE_N=configs[dtype]["BLOCK_SIZE_N"],  #
-        BLOCK_SIZE_K=configs[dtype]["BLOCK_SIZE_K"],  #
-        GROUP_SIZE_M=configs[dtype]["GROUP_SIZE_M"],  #
-        FP8_OUTPUT=dtype == torch.float8_e4m3fn,  #
-        NUM_SMS=NUM_SMS,  #
-        num_stages=configs[dtype]["num_stages"],  #
-        num_warps=configs[dtype]["num_warps"],  #
-    )
+    
+    if override:
+        override_bin = matmul_kernel_tma_persistent_OVERRIDE[grid](
+            desc_a, desc_b, desc_c,  #
+            M, N, K,  #
+            BLOCK_SIZE_M=configs[dtype]["BLOCK_SIZE_M"],  #
+            BLOCK_SIZE_N=configs[dtype]["BLOCK_SIZE_N"],  #
+            BLOCK_SIZE_K=configs[dtype]["BLOCK_SIZE_K"],  #
+            GROUP_SIZE_M=configs[dtype]["GROUP_SIZE_M"],  #
+            FP8_OUTPUT=dtype == torch.float8_e4m3fn,  #
+            NUM_SMS=NUM_SMS,  #
+            num_stages=configs[dtype]["num_stages"],  #
+            num_warps=configs[dtype]["num_warps"],  #
+        )
+        print(f"override hash: {override_bin.metadata.hash}")
+    else:
+        matmul_kernel_tma_persistent[grid](
+            desc_a, desc_b, desc_c,  #
+            M, N, K,  #
+            BLOCK_SIZE_M=configs[dtype]["BLOCK_SIZE_M"],  #
+            BLOCK_SIZE_N=configs[dtype]["BLOCK_SIZE_N"],  #
+            BLOCK_SIZE_K=configs[dtype]["BLOCK_SIZE_K"],  #
+            GROUP_SIZE_M=configs[dtype]["GROUP_SIZE_M"],  #
+            FP8_OUTPUT=dtype == torch.float8_e4m3fn,  #
+            NUM_SMS=NUM_SMS,  #
+            num_stages=configs[dtype]["num_stages"],  #
+            num_warps=configs[dtype]["num_warps"],  #
+        )
     return c
 
 
@@ -391,31 +469,12 @@ def bench(K, dtype, reps=10):
     N = 8192
     a = torch.randn((M, K), device="cuda", dtype=torch.float16).to(dtype)
     b = torch.randn((K, N), device="cuda", dtype=torch.float16).to(dtype)
-
     b = b.T.contiguous()
+    c = torch.zeros((M, N), device=a.device, dtype=dtype)
 
-    proton.activate(0)
-
-    if cublas is not None:
-        for _ in range(reps):
-            cublas_matmul(a, b)
-        time.sleep(0.01)
-    if dtype == torch.float16:
-        for _ in range(reps):
-            torch_matmul(a, b)
-            time.sleep(0.01)
-    for _ in range(reps):
-        matmul(a, b.T)
-        time.sleep(0.01)
-    for _ in range(reps):
-        matmul_persistent(a, b.T)
-        time.sleep(0.01)
-    if supports_tma():
-        for _ in range(reps):
-            matmul_tma_persistent(a, b)
-            time.sleep(0.01)
-
-    proton.deactivate(0)
+    desc_a, desc_b, desc_c = tma_setup(a, b, c, M, N, K)
+    
+    matmul_tma_persistent(a, b, c, desc_a, desc_b, desc_c)
 
 
 def validate(M, N, K, dtype):
@@ -427,7 +486,14 @@ def validate(M, N, K, dtype):
     cublas_result = cublas_matmul(a, b) if cublas is not None else None
     naive_result = matmul(a, b.T)
     persistent_result = matmul_persistent(a, b.T)
-    tma_persistent_result = matmul_tma_persistent(a, b) if supports_tma() else None
+
+    tma_persistent_result = torch.zeros((M, N), device=a.device, dtype=dtype)
+    desc_a, desc_b, desc_result = tma_setup(a, b, tma_persistent_result, M, N, K)
+    matmul_tma_persistent(a, b, tma_persistent_result, desc_a, desc_b, desc_result) if supports_tma() else None
+    
+    tma_persistent_result_OVERRIDE = torch.zeros((M, N), device=a.device, dtype=dtype)
+    desc_a, desc_b, desc_result = tma_setup(a, b, tma_persistent_result_OVERRIDE, M, N, K)
+    matmul_tma_persistent(a, b, tma_persistent_result_OVERRIDE, desc_a, desc_b, desc_result, override=True) if supports_tma() else None
 
     if torch_result is not None:
         naive_vs_torch = "✅" if torch.allclose(naive_result.to(torch.float16), torch_result.to(torch.float16),
@@ -440,6 +506,9 @@ def validate(M, N, K, dtype):
     if tma_persistent_result is not None:
         naive_vs_tma_persistent = "✅" if torch.allclose(cublas_result.to(torch.float16),
                                                         tma_persistent_result.to(torch.float16), atol=1.0) else "❌"
+    if tma_persistent_result is not None:
+        naive_vs_tma_persistent_OVERRIDE = "✅" if torch.allclose(cublas_result.to(torch.float16),
+                                                        tma_persistent_result_OVERRIDE.to(torch.float16), atol=1.0) else "❌"
     print(f"M={M}, N={N}, K={K} verification naive vs: ", end="")
     if torch_result is not None:
         print(f"torch: {naive_vs_torch} ", end="")
@@ -448,6 +517,8 @@ def validate(M, N, K, dtype):
     print(f"persistent: {naive_vs_persistent} ", end="")
     if tma_persistent_result is not None:
         print(f"TMA persistent: {naive_vs_tma_persistent}")
+    if tma_persistent_result_OVERRIDE is not None:
+        print(f"TMA persistent override: {naive_vs_tma_persistent_OVERRIDE}")
 
 
 if __name__ == "__main__":
